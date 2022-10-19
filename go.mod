e0f35e0fc754b8ec9f717c165fa222d648ef6262
Mario Finelli <mario@finel.li>
2022-10-19 18:10:59 -0400

    Start building initial user interface

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/mfinelli/cui
 
 go 1.19
+
+require (
+	github.com/gdamore/tcell/v2 v2.5.3
+	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
+)
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+func main() {
+	app := tview.NewApplication()
+
+	methods := []string{
+		http.MethodDelete,
+		http.MethodHead,
+		http.MethodGet,
+		http.MethodOptions,
+		http.MethodPatch,
+		http.MethodPost,
+		http.MethodPut,
+	}
+	methodGet := 2 // methods is zero-indexed
+
+	methodDropdown := tview.NewDropDown().SetOptions(methods, nil).SetCurrentOption(methodGet)
+	urlInput := tview.NewInputField().SetLabel("URL: ")
+
+	methodAndUrl := tview.NewFlex().
+		AddItem(methodDropdown, 10, 0, false).
+		AddItem(urlInput, 0, 1, false)
+
+	newRequest := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(methodAndUrl, 1, 0, false).
+		AddItem(tview.NewTextView().SetText("request"), 0, 1, false).
+		AddItem(tview.NewTextView().SetText("response"), 0, 1, false)
+
+	newRequest.SetBorder(true).SetTitle(" New Request ")
+
+	inner := tview.NewFlex().
+		AddItem(tview.NewBox().SetBorder(true).SetTitle(" Request History "), 0, 1, false).
+		AddItem(newRequest, 0, 3, false)
+
+	header := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText("cUI v1.0.0")
+
+	main := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(header, 1, 0, false).
+		AddItem(inner, 0, 1, false).
+		AddItem(tview.NewTextView().SetText(" (q) quit  (m) set method  (u) set url"), 1, 0, false)
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 { // q
+			app.Stop()
+		}
+
+		return event
+	})
+
+	if err := app.SetRoot(main, true).Run(); err != nil {
+		panic(err)
+	}
+}
