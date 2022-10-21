84efeb7bd36559b78851aec4e2055ec0363365c3
Mario Finelli <mario@finel.li>
2022-10-21 17:10:40 -0400

    Add basic syntax highlighting
    Resolves #1

--- go.mod
+++ go.mod
@@ -3,11 +3,13 @@ module github.com/mfinelli/cui
 go 1.19
 
 require (
+	github.com/alecthomas/chroma/v2 v2.3.0
 	github.com/gdamore/tcell/v2 v2.5.3
 	github.com/rivo/tview v0.0.0-20220916081518-2e69b7385a37
 )
 
 require (
+	github.com/dlclark/regexp2 v1.4.0 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
--- request.go
+++ request.go
@@ -5,7 +5,11 @@ import (
 	"io/ioutil"
 	// "log"
 	"net/http"
+	"strings"
 
+	"github.com/alecthomas/chroma/v2/formatters"
+	"github.com/alecthomas/chroma/v2/lexers"
+	"github.com/alecthomas/chroma/v2/styles"
 	"github.com/rivo/tview"
 )
 
@@ -18,6 +22,9 @@ func sendRequest(req cuiRequest, cui *cuiApp, hasResponse *bool) error {
 	client := &http.Client{}
 	cui.ResponseBody.Clear()
 	cui.ResponseHeaders.Clear()
+
+	highlight := false
+	highlightLang := ""
 
 	r, err := http.NewRequest(req.Method, req.URL, nil)
 	if err != nil {
@@ -32,11 +39,10 @@ func sendRequest(req cuiRequest, cui *cuiApp, hasResponse *bool) error {
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	cui.ResponseStatus.SetText(fmt.Sprintf("Status: %d", res.StatusCode))
-	cui.ResponseBody.SetText(string(body)).ScrollToBeginning()
 
 	cui.ResponseHeaders.SetCell(0, 0, tview.NewTableCell("Header"))
 	cui.ResponseHeaders.SetCell(0, 1, tview.NewTableCell("Value"))
@@ -44,10 +50,40 @@ func sendRequest(req cuiRequest, cui *cuiApp, hasResponse *bool) error {
 	i := 1
 	for k, v := range res.Header {
 		for _, vv := range v {
+			if strings.EqualFold("content-type", k) {
+				if strings.HasPrefix(strings.ToLower(vv), "text/html") {
+					highlight = true
+					highlightLang = "html"
+				} else if strings.HasPrefix(strings.ToLower(vv), "application/json") {
+					highlight = true
+					highlightLang = "json"
+				}
+			}
+
 			cui.ResponseHeaders.SetCell(i, 0, tview.NewTableCell(k))
 			cui.ResponseHeaders.SetCell(i, 1, tview.NewTableCell(vv))
 			i += 1
 		}
+	}
+
+	if highlight {
+		// TODO: need to call app.Draw() in a SetChangedFunc?
+		cui.ResponseBody.SetDynamicColors(true)
+
+		lexer := lexers.Get(highlightLang)
+		style := styles.Get("fruity")
+		formatter := formatters.Get("terminal")
+		iterator, err := lexer.Tokenise(nil, string(body))
+
+		w := tview.ANSIWriter(cui.ResponseBody)
+		err = formatter.Format(w, style, iterator)
+		if err != nil {
+			return err
+		}
+
+		cui.ResponseBody.ScrollToBeginning()
+	} else {
+		cui.ResponseBody.SetText(string(body)).ScrollToBeginning()
 	}
 
 	*hasResponse = true
